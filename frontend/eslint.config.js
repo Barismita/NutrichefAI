62fb337358a085e9e3e7407146c915233b1cd8dc
barismita <bsingh86@lululemon.com>
2026-07-26 02:07:07 +0530

    feature: set up frontend foundation with Vite, MUI, routing and tooling

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,21 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+    globalIgnores(["dist", "node_modules"]),
+
+    {
+        files: ["**/*.{js,jsx}"],
+
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            eslintConfigPrettier,
+        ],
+
+        plugins: {
+            prettier: prettierPlugin,
+        },
+
+        rules: {
+            "prettier/prettier": "error",
+        },
+
+        languageOptions: {
+            globals: globals.browser,
+
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
     },
-  },
-])
+]);
